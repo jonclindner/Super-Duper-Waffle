11dcd48077abffa403abf45c83556960b02cefa3
jonclindner <jonathan.c.lindner@gmail.com>
2023-01-23 14:16:07 -0700

    added basic player functionality (without gravity)

---
+++ script.js
@@ -0,0 +1,33 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// .split() takes strings and turns them into an ordered array. 
+
+//https://stackoverflow.com/questions/13272406/convert-string-with-commas-to-array
+//Another source to turn stings into arrays.
+
+
+
+const plates = document.querySelectorAll('.plate')
+let currentPlayer = 0
+
+const playGame = () => {
+for (let i = 0; i < plates.length; i++) {
+    plates[i].onclick = () => {
+        if (currentPlayer === 0) {
+        //Reners the "playerOne" color to the selected plate
+        // from: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+            plates[i].classList.add('playerOne')
+            currentPlayer = 1
+            playGame()
+            console.log(currentPlayer)
+        }
+        else if (currentPlayer === 1) {
+            plates[i].classList.add('playerTwo')
+            currentPlayer = 0 
+            playGame() 
+            console.log(currentPlayer)
+        }
+    }
+}
+}
+
+playGame()
